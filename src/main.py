545db46ca574260c413e43ae234d4e4b5af1fa5c
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-07-09 21:30:26 -0600

    Co-authored-by: TheManWhoDoesTheStuffAndJunk <TheManWhoDoesTheStuffAndJunk@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,3 +1,30 @@
 # Summer Game Project
-# import pygame
-# need to install pygame
+
+import pygame
+pygame.init()
+
+def main():
+    #Create Screen
+    screen = pygame.display.set_mode((1280, 720))
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+    
+        screen.fill("blue")
+        pygame.draw.rect(screen,"white",(100,100,100,100),border_radius=5)
+
+        pygame.display.flip()
+
+        clock.tick(60)
+    
+    pygame.quit()
+
+    #Create Buttons
+
+
+
+main()
