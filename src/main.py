0ec50e8d29b101387456b7255e61a14f69008832
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-07-10 14:11:28 -0600

    Main Menu Buttons and UI added

--- src/main.py
+++ src/main.py
@@ -8,14 +8,46 @@ def main():
     screen = pygame.display.set_mode((1530, 825))
     clock = pygame.time.Clock()
     running = True
+    title_font = pygame.font.Font("src/files/BlackOpsOne-Regular.ttf",128)
+    title = title_font.render("PC's Games", True, (0,50,255))
+
+    right_arrow_coords = (1200,475)
+    left_arrow_coords = (300,475)
 
     while running:
+    
+        screen.fill("black")
+        
+        #Title
+        pygame.draw.rect(screen,"darkblue",(250,50,1000,150),width=8,border_radius=5)
+        screen.blit(title, (340,50))
+        
+        #Game Placeholder Box
+        pygame.draw.rect(screen,"gold", (450,350,600,300),width=8,border_radius=5)
+
+        #Right Arrow
+        x, y = right_arrow_coords
+        pygame.draw.polygon(screen,"red",[right_arrow_coords,(x,y+25),(x+50,y+25),(x+50,y+40),(x+75,y+12.5),(x+50,y-15),(x+50,y)])
+
+        #Left Arrow
+        x,y = left_arrow_coords
+        pygame.draw.polygon(screen,"red",[left_arrow_coords,(x,y+25),(x-50,y+25),(x-50,y+40),(x-75,y+12.5),(x-50,y-15),(x-50,y)])
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-    
-        screen.fill("black")
-        pygame.draw.rect(screen,"white",(100,100,100,100),border_radius=5)
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                ex,ey = event.pos
+                #Right Arrow Checking
+                if ex >= 1100 and ex <= 1400:
+                    if ey >= 450 and ey <= 525:
+                        print("Right Arrow Clicked")
+                
+                #Left Arrow Checking
+                if ex >= 175 and ex <= 325:
+                    if ey >= 450 and ey <= 525:
+                        print("Left Arrow Clicked")
 
         pygame.display.flip()
 
