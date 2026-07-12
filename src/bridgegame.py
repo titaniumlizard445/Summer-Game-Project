43a0acb9ba507170b9deaab8fb8c4431ac038620
Charles mcBarles <charlightning22@gmail.com>
2026-07-11 21:46:58 -0600

    added a bunch of main menu and other stuff but its not finished

--- src/bridgegame.py
+++ src/bridgegame.py
@@ -1,9 +1,49 @@
 import pygame
 pygame.init()
-
+screen = pygame.display.set_mode((1530, 825))
+clock = pygame.time.Clock()
+running = True
+curr = "main menu"
+font = pygame.font.Font("src/files/BlackOpsOne-Regular.ttf",128)
 
 def main():
-    clock = pygame.time.Clock()
+    title = font.render("BRIDGE", True, (0,50,255))
+
+    while running:
+        for event in pygame.event.get():
+            if event == pygame.QUIT():
+                running = False
+
+        match curr:
+            case "main menu":
+                main_menu()
+            # case settings:
+
+            case _:
+                print("Invalid value given for match case (" + curr + "). Changed back to main menu.")
+                curr = "main menu"
+
+
+
+
+
+
+
+        pygame.display.flip()
+        clock.tick(60) # limit fps to 60
+    
+    # quit if not running            
+    pygame.QUIT()
+
+
+
+
+def main_menu():
+    screen.fill("black")
+    
+
+    
+    
 
 
 
@@ -15,7 +55,6 @@ def main():
 
 
 
-
-
+main()
 
 
