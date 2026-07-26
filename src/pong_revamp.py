4104deafbc9384b16024dba01b9ceadfb043bf50
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-07-25 21:18:02 -0600

    Explanation and Play button things created

--- src/pong_revamp.py
+++ src/pong_revamp.py
@@ -18,6 +18,10 @@ def pong_menu():
     secondary_font = pygame.font.Font("src/files/BlackOpsOne-Regular.ttf",40)
     scoreboard_title_text = secondary_font.render("Single Player",True,"blue")
     scoreboard = secondary_font.render("Scoreboard",True,"blue")
+    play_button_text1 = title_font.render("Play Regular 2P",True,"green")
+    play_button_text2 = title_font.render("Play Singleplayer",True,"green")
+    explanation_message = "This Version of \n Pong includes \n Powerups and \n abilities to \n spice up \n the timeless \n classic, Pong! \n There is also a \n Single Player \n Version which \n Only Uses \n P1 Controls"
+
 
     while running:
 
@@ -36,6 +40,18 @@ def pong_menu():
         screen.blit(scoreboard_title_text,(1125,40))
         screen.blit(scoreboard,(1130,75))
         pygame.draw.rect(screen,"darkblue",(1100,150,350,500),width=8,border_radius=5)
+
+        #Play Buttons
+        pygame.draw.rect(screen,"darkgreen",(470,200,580,150),width=8,border_radius=5)
+        screen.blit(play_button_text1,(500,230))
+
+        pygame.draw.rect(screen,"darkgreen",(470,400,580,150),width=8,border_radius=5)
+        screen.blit(play_button_text2,(475,430))
+
+        #Explanation Box
+        pygame.draw.rect(screen,"darkblue",(50,150,350,500),width=8,border_radius=5)
+        for line,x in enumerate(explanation_message.split(" \n ")):
+            screen.blit(secondary_font.render(x,True,"blue"),(60,170+(line*35)))
 
 
 
