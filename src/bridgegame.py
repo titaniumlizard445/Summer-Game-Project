c0d689994bc8c53597b727a107f4fa5cc4510f10
Charles mcBarles <charlightning22@gmail.com>
2026-07-25 21:00:48 -0600

    insert commit message here

--- src/bridgegame.py
+++ src/bridgegame.py
@@ -10,15 +10,25 @@ def bridge_main():
     font = pygame.font.Font("src/files/BlackOpsOne-Regular.ttf",128)
     title = font.render("BRIDGE", True, (0,50,255))
 
+
     while running:
         for event in pygame.event.get():
             if event == pygame.QUIT:
                 running = False
+            # elif event.type == pygame.KEYDOWN:
+                # print("key pressed")
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                print("clicked")
+                if (curr == "main menu"):
+                    curr = "game"
 
         match curr:
             case "main menu":
-                main_menu(screen)
-            # case settings:
+                main_menu(screen, title)
+            
+            # case "settings":
+            # case "game":
+
 
             case _:
                 print("Invalid value given for match case (" + curr + "). Changed back to main menu.")
@@ -39,8 +49,9 @@ def bridge_main():
 
 
 
-def main_menu(screen):
+def main_menu(screen, title):
     screen.fill("black")
+    screen.blit(title, (340, 50))
     
 
     
